04a4107adb0a8ea87d84c3cd9ee23dc93ac1c837
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-08-15 15:11:25 -0400

    MPG Calculator

---
+++ week2/JadonLama_Chapter2.py
@@ -0,0 +1,9 @@
+# MPG Calculator 
+# asking for num of miles driven and gallons used
+miles_driven = float(input('Enter miles driven: '))
+gallons_used = float(input('Enter gallons used: '))
+
+# calculate and display miles per gallon
+mpg = miles_driven / gallons_used 
+print('Miles per gallon:', mpg)
+
