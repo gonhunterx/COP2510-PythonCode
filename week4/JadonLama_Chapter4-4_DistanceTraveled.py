eab43753bd5f7a78f7c24c51a7287252c39ff62e
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-09-11 14:51:25 -0400

    Update
    Reconfigured rectangle comparison program. Created Distance Traveled program.

--- week4/JadonLama_Chapter4-4_DistanceTraveled.py
+++ week4/JadonLama_Chapter4-4_DistanceTraveled.py
@@ -2,16 +2,29 @@
 # distance = speed * time
 
 
-# Variables
-vehicle_speed = int(input("What is the vehicles speed(MPH)?: "))
-hours_traveled = int(input("How many hours did you travel?: "))
-
-
-# function for calculating average speed per hour based on speed * time traveled
-def speedCalculator():
+# function for calculating average distance per hour based on speed * time traveled
+def distanceCalculator(hours_traveled, vehicle_speed):
     for inital_hour in range(hours_traveled):
-        speedPerHour = vehicle_speed / hours_traveled
-        print(f"Hour {inital_hour+1}: Traveling at {speedPerHour} MPH")
+        distance = vehicle_speed / hours_traveled
+        print(f"Hour  Distance Traveled\n{inital_hour+1}     {distance} miles")
 
 
-speedCalculator()
+def main():
+    # creating a while loop to keep the program running until a break is initiated
+    while True:
+        # Variables
+        vehicle_speed = int(input("What is the vehicles speed(MPH)?: "))
+        hours_traveled = int(input("How many hours did you travel?: "))
+        # calling the speedCalculator function and passing parameters to it for the calculation with the given inputs as their values.
+        distanceCalculator(hours_traveled, vehicle_speed)
+        # adding a choice to continue. this makes it so once the main program has
+        # run the string below will appear and it assumes that if you type anything
+        # besides yes that you are done.
+        choice = input("Do you want to go again? yes or no: ")
+        if choice.lower() != "yes":
+            break
+
+
+# calling the main function
+if __name__ == "__main__":
+    main()
