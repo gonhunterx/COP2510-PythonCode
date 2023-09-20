d5e88f547bc1930e4de30ae8d640df503e654c0a
Jadon Lama <Jadonlama357@gmail.com>
2023-09-20 10:03:58 -0400

    Create radianOfAngles.py

---
+++ SideProjects/radianOfAngles.py
@@ -0,0 +1,12 @@
+import math 
+
+def convertAngle(num):
+    convertedValue = math.radians(num)
+    return convertedValue
+    
+    
+def main():
+    angle = int(input("What is the angle you want to convert to radians?: "))
+    result = convertAngle(angle)
+    return result
+    print(result)
