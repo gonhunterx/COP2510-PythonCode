60bc05ca4b140b7bd8e4118c68e834827615dbdc
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-09-29 20:59:02 -0400

    Upload Update

--- week6/JadonLama_Chapter6_Assignment10.py
+++ week6/JadonLama_Chapter6_Assignment10.py
@@ -2,17 +2,21 @@
 # importing sys for easy exit inside or outside of loops
 import sys
 
-gulf_data = []
-
 
 # main menu function
 def menu():
+    # styling for the menu :)
     print("================")
+    # list of options to select from
     print("1. Enter Data")
     print("2. Read Data")
     print("3. Clear Data")
+    # creating an exit from the program
     print("4. Quit")
-    choice = input("Please select an option: ")
+    # creating the variable for the main menu choice.
+    choice = input("Please select an option [1-4]: ")
+    # if statement for the logic behind the menu by entering numbers in the form of strings then just reading them
+    # this will determine which function to call based on user input.
     if choice == "1":
         edit_file()
     elif choice == "2":
@@ -20,9 +24,12 @@ def menu():
     elif choice == "3":
         clear_data()
     elif choice == "4":
+        # exits program.
         sys.exit()
+    # catching if anything else is entered.
     else:
         print("Invalid choice.")
+        menu()
 
 
 # function to clear the data.
@@ -34,30 +41,46 @@ def clear_data():
         # try opening and writing over it (when the file is opened in "w" then closed
         # it truncates the existing content)
         try:
+            # this line opens and closes the file in write mode which will clear it.
             open("gulf.txt", "w").close()
-            print("Cleared")
+            print("Cleared.")
+            menu()
         # error handling while trying to truncate the file.
         except Exception as e:
+            # using Exception and naming it 'e' to easily print it rather than typing Exception again.
             print(f"An error occured: {e}")
     else:
+        # if no is selected we are going back the the main menu
         print("Data not cleared.")
+        # right now once this else statement executes you are sent directly into adding a new player name and score.
+        # Fixed by adding the menu function here it sends you back
+        menu()
+        # using menu() in else statements works well because then you can essentially default back to the menu
 
 
 def edit_file():
-    while True:
-        # using 'a' to append data to the file
-        with open("gulf.txt", "a") as gulfscoresFile:
-            player_name = input("What is the player's name?: ")
-            gulf_score = input("What is the associated score?: ")
-            # write the data in columns with commas
-            gulfscoresFile.write(f"{player_name}, {gulf_score}\n")
-            print("Saved.")
-
-        again = input("Do you want to enter more data? (yes or no): ")
-        if again.lower() == "yes":
-            edit_file()
-        else:
-            menu()
+    # using 'a' to append data to the file
+    with open("gulf.txt", "a") as gulfscoresFile:
+        # CREATING the player_name and gulf_score variables.
+        player_name = input("What is the player's name?: ")
+        gulf_score = input("What is the associated score?: ")
+        # trying to create the logic for if someone enters no data.
+        # if player_name != "" and gulf_score != "":
+        # write the data in columns with commas
+        gulfscoresFile.write(f"{player_name}, {gulf_score}\n")
+        print("Saved.")
+    # again variable is created to store the input of the user to determine if they want to go again.
+    # must take this part of the function outside of the with statement. So it will go to the second part AFTER already closing the file.
+    again = input("Do you want to enter more data? (yes or no): ")
+    # checking if the user wants to enter more data.
+    if again.lower() == "yes":
+        # going to the edit_file() function to enter more data.
+        edit_file()
+    else:
+        # returning to menu() with the function call
+        menu()
+        # else:
+        #     print("Player name and score can not be empty.")
 
 
 def read_file():
@@ -87,13 +110,3 @@ def main():
 
 # call the main function
 main()
-
-
-# LIST OF ADJUSTMENTS
-# - create an error catch for when there are two of the same name in the list.
-# -
-#
-#
-#
-#
-#
