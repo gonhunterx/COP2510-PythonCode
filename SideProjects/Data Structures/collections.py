163b631bb07e38b50d4441c1b0795993da8c5cc9
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-10-02 23:30:05 -0400

    update

---
+++ SideProjects/Data Structures/collections.py
@@ -0,0 +1,13 @@
+# collections = single "variable" used to store multiple values
+# List  = [] ordered and changeable. Duplicates OK
+# Set   = {} unordered and immutable, but Add/Remove OK. NO duplicates
+# Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+
+fruits = ["apple", "orange", "banana", "coconut"]
+# print(dir(fruits))
+print(help(fruits))
+
+# print(fruits[::-1])
+
+# for fruit in fruits:
+#     print(fruit)
