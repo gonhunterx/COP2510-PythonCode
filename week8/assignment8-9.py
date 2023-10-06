f9fbaeba375f45425c8d5a7ed33a562514c88666
Jadon Lama <Jadonlama357@gmail.com>
2023-10-06 16:34:03 -0400

    Update assignment8-9.py

--- week8/assignment8-9.py
+++ week8/assignment8-9.py
@@ -2,6 +2,17 @@
 import sys
 
 
+def results():
+    # creating var for the functions to process
+    answer = input("What is the word: ")
+    print("===================")
+    # calling functions and printing their outputs
+    print(f"Vowels: {vowel_counter(answer)}")
+    print(f"Consonants: {consonant_counter(answer)}")
+    print("===================")
+    main()
+
+
 def vowel_counter(word):
     # vowel list
     vowels = "aeiou"
@@ -17,19 +28,6 @@ def vowel_counter(word):
     return v_total
 
 
-def menu():
-    print("Main Menu")
-    print("1. Go again")
-    print("2. Quit")
-    choice = input("Enter a choice: ")
-    if choice == "1":
-        main()
-    elif choice == "2":
-        sys.exit()
-    else:
-        menu()
-
-
 def consonant_counter(word):
     # vowel list
     vowels = "aeiou"
@@ -39,6 +37,7 @@ def consonant_counter(word):
     for ch in word:
         # if letter is a consonant add to counter
         # adding .lower() so it takes more input options
+        # isalpha means is alphabet
         if ch.lower() not in vowels and ch.isalpha():
             c_total += 1
     # returning the updated total value
@@ -46,17 +45,29 @@ def consonant_counter(word):
 
 
 def main():
-    # creating var for the functions to process
-    answer = input("What is the word: ")
-    # calling functions and printing their outputs
-    print(f"Vowels: {vowel_counter(answer)}")
-    print(f"Consonants: {consonant_counter(answer)}")
-    menu()
+    print("Main menu")
+    print("1. Vowel and Consonant Counter")
+    print("2. Exit Program")
+    choice = input("Enter a choice: ")
+    if choice == "1":
+        results()
+    elif choice == "2":
+        sys.exit()
+    else:
+        print("Invalid Input.")
+        main()
 
 
-menu()
+# calling the main function
+if __name__ == "__main__":
+    main()
 
 
 # could be like this with arguments
 # if you did not use an input for word
 # print(vowel_counter("banana"))
+
+
+# NOTES
+# I think that what is in the main function should be its own function and the results should
+# just be in the main function.
